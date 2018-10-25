d40331084bf89cac7671c34fecfb1bbda38f49a6
Lyndon Bibera <lyndon.bibera@boltoninternational.com>
2018-10-25 23:17:39 +0800

    generated project from spring initializer

---
+++ src/main/java/ph/homecredit/weatherlog/api/WeatherLogApiApplication.java
@@ -0,0 +1,12 @@
+package ph.homecredit.weatherlog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherLogApiApplication.class, args);
+	}
+}
